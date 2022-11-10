2696c1952fd0a77234a0e2ce175e0031dce00839
Cameron Leslie <cameronleslie@gmail.com>
2022-11-10 10:25:40 -0800

    Missed some datatsets.

--- test/parser-ccda/ccd-medical-equipment-section/test-medical-equipment-section.js
+++ test/parser-ccda/ccd-medical-equipment-section/test-medical-equipment-section.js
@@ -5,14 +5,14 @@ describe('medical equipment section', function () {
   it('Non-medicinal supply activity', function () {
     var xmlfile = fs.readFileSync(__dirname + '/implant-udi-organizer.xml', 'utf-8').toString();
     var result = bb.parse(xmlfile);
-    console.log(JSON.stringify(result.data.medical_equipment_section, null ,"  "))
+    // console.log(JSON.stringify(result.data.medical_equipment_section, null ,"  "))
     expect(result.data.medical_equipment_section).toBeDefined();
   });
 
   it('implant organizer unknown', function () {
     var xmlfile = fs.readFileSync(__dirname + '/implant-udi-unknown.xml', 'utf-8').toString();
     var result = bb.parse(xmlfile);
-    console.log(JSON.stringify(result.data.medical_equipment_section, null ,"  "))
+    // console.log(JSON.stringify(result.data.medical_equipment_section, null ,"  "))
     expect(result.data.medical_equipment_section).toBeDefined();
   });
 
@@ -26,21 +26,21 @@ describe('medical equipment section', function () {
   it('multi implant', function () {
     var xmlfile = fs.readFileSync(__dirname + '/multiple-implants.xml', 'utf-8').toString();
     var result = bb.parse(xmlfile);
-    console.log(JSON.stringify(result.data.medical_equipment_section, null ,"  "))
+    // console.log(JSON.stringify(result.data.medical_equipment_section, null ,"  "))
     expect(result.data.medical_equipment_section).toBeDefined();
   });
 
   it('no implanted devices', function () {
     var xmlfile = fs.readFileSync(__dirname + '/no-implanted-devices.xml', 'utf-8').toString();
     var result = bb.parse(xmlfile);
-    console.log(JSON.stringify(result.data.medical_equipment_section, null ,"  "))
+    // console.log(JSON.stringify(result.data.medical_equipment_section, null ,"  "))
     expect(result.data.medical_equipment_section).toBeDefined();
   });
 
   it('non-medicinal supply', function () {
     var xmlfile = fs.readFileSync(__dirname + '/non-medicinal-supply-cane-and-eyeglasses.xml', 'utf-8').toString();
     var result = bb.parse(xmlfile);
-    console.log(JSON.stringify(result.data.medical_equipment_section, null ,"  "))
+    // console.log(JSON.stringify(result.data.medical_equipment_section, null ,"  "))
     expect(result.data.medical_equipment_section).toBeDefined();
   });
 
